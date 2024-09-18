109dbf050f2ec4121c0a4edcbf0816092b2f18e1
Farok Hossain <farokafs7@gmail.com>
2024-09-18 21:24:52 +0600

    leapYear added

--- leapYear.js
+++ leapYear.js
@@ -1,10 +1,13 @@
 const LeapYear = (year) => {
   if (year % 100 == 0) {
-  } else if (year % 4 == 0 && year % 400 == 0) {
+    console.log("not leap  year");
+  } else if (year % 4 == 0) {
+    console.log("It is Leap Year");
+  } else if (year % 400 == 0) {
     console.log("It is Leap Year");
   } else {
-    console.log("It is not Leap Year");
+    console.log("It is not leap year");
   }
 };
 
-const result = LeapYear(2004);
+const result = LeapYear(2003);
