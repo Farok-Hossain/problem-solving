5d6a906d7cf58c38704b0dca31db80789ea0efcb
Farok Hossain <farokafs7@gmail.com>
2024-09-19 22:16:10 +0600

    updated

---
+++ removeCharacter.js
@@ -0,0 +1,13 @@
+const remove = (cha) => {
+  return cha.slice(0, -1);
+  // console.log(cha.slice(0, -1));
+};
+const modifiedValue = remove("Farok Hossain");
+console.log(modifiedValue);
+
+const str = "Farok Hossain";
+const p = str.substring(0, 0);
+console.log(p);
+
+const g = "Python";
+console.log(g.length);
