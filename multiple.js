7c2662f7acd698a3759e8c7cb3711be91171f45c
Farok Hossain <farokafs7@gmail.com>
2024-09-20 15:36:26 +0600

    multiple added

---
+++ multiple.js
@@ -0,0 +1,12 @@
+const number = (posN) => {
+  if (posN % 3 == 0 || posN % 7 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(number(10));
+console.log(number(14));
+console.log(number(15));
+console.log(number(21));
