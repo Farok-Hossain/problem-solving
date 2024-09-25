66dee14a14a6c7760d45966cbf74d38d57a541e5
Farok Hossain <farokafs7@gmail.com>
2024-09-25 18:43:47 +0600

    updated

--- integer.js
+++ integer.js
@@ -27,3 +27,16 @@ console.log(largest(3, 66, 77));
 console.log(largest(69, 55, 77));
 console.log(largest(55, 88, 77));
 console.log(largest(45, 45, 45));
+
+const lastDigit = (f, g, h) => {
+  if (f > 0 && g > 0 && h > 0) {
+    return f % 10 === g % 10 && f % 10 === h % 10 && g % 10 === h % 10;
+  } else {
+    return false;
+  }
+};
+console.log(lastDigit(12, 34, 55));
+console.log(lastDigit(10, 50, 90));
+console.log(lastDigit(88, 30, 50));
+console.log(lastDigit(20, 33, 53));
+console.log(lastDigit(-20, 30, 50));
