c4c8345c599ad92ec8540bb2ea952969c3faa207
Farok Hossain <farokafs7@gmail.com>
2024-09-30 01:24:23 +0600

    sort added

---
+++ sort.js
@@ -0,0 +1,6 @@
+let names = ["Farok", "Azad", "Shamim", "Rasedul", "Nazim"];
+console.log(names.sort());
+
+let names2 = "programming";
+names2 = names2.split("").sort().join(" ");
+console.log(names2);
