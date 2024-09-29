fa7c0bd692006e435f85269aa286420d11757cf3
Farok Hossain <farokafs7@gmail.com>
2024-09-29 17:15:10 +0600

    updated

--- equalLD.js
+++ equalLD.js
@@ -5,3 +5,23 @@ const equalLastDigit = (m, n, o) => {
 console.log(equalLastDigit(2, 4, 23));
 console.log(equalLastDigit(22, 42, 52));
 console.log(equalLastDigit(23, 45, 95));
+
+const greaterLess = (p, q, r) => {
+  return (
+    (p >= 20 && (p < q || p < r)) ||
+    (q >= 20 && (q < p || q < r)) ||
+    (r >= 20 && (r < p || r < q))
+  );
+};
+
+console.log(greaterLess(10, 12, 15));
+console.log(greaterLess(22, 23, 77));
+console.log(greaterLess(55, 45, 43));
+
+const sumDifference = (x, y) => {
+  return x == 15 || y == 15 || x + y == 15 || x - y == 15;
+};
+
+console.log("sumDiff");
+console.log(sumDifference(13, 15));
+console.log(sumDifference(18, 3));
