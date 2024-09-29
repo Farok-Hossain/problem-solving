fb6c09e0bbbd771b8758ce6ff150aad97e1d00c9
Farok Hossain <farokafs7@gmail.com>
2024-09-29 23:29:12 +0600

    reverse updated

--- equalLD.js
+++ equalLD.js
@@ -44,7 +44,3 @@ console.log("multiple");
 console.log(multipleOfNumbers(7, 20));
 console.log(multipleOfNumbers(22, 28));
 console.log(multipleOfNumbers(4, 2));
-
-// reverse string
-let name = "farok hossain";
-console.log(name.split("").reverse().join(""));
--- reverse.js
+++ reverse.js
@@ -1,2 +1,16 @@
-const name = "Farok Hossain";
-console.log(name.split("").reverse().join(""));
+const name1 = "Farok Hossain12";
+console.log(name1.split("").reverse().join(""));
+
+const name2 = "Farok Hossain12";
+console.log(name2.split(" ").reverse().join(""));
+
+const n = "farok Hossain afs";
+const s = n.split(" ");
+console.log(s);
+
+const name3 = "Farok";
+const s3 = name3.split("");
+console.log(s3);
+const j = s3.join("");
+const j2 = s3.join(" ");
+console.log(j, j2);
