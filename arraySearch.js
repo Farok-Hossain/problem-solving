dd5a9bf5a6c5634ccf0d43e1f02ae17477a9c82d
Farok Hossain <farokafs7@gmail.com>
2024-10-04 22:32:07 +0600

    arraySearch.js added

---
+++ arraySearch.js
@@ -0,0 +1,6 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango", "Apple"];
+
+let position = fruits.indexOf("Orange") + 1;
+console.log(position);
+let position2 = fruits.lastIndexOf("Apple") + 1;
+console.log(position2);
