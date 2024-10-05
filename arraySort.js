89819934eab915eb19b048b253a72cfef904f950
Farok Hossain <farokafs7@gmail.com>
2024-10-05 21:36:17 +0600

    updated

--- arraySort.js
+++ arraySort.js
@@ -1,3 +1,8 @@
 const fruits = ["Mango", "Banana", "Orange", "Apple", "Papaya"];
 console.log(fruits.sort());
 console.log(fruits.reverse());
+
+const points = [40, 67, 2, 5, 54, 29, 34, 6];
+console.log(points.sort((a, b) => a - b));
+
+console.log(points.sort((a, b) => b - a));
