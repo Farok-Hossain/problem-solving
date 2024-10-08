53a5d9fe9bbc4b4c3a0227a3cee66798eb472be8
Farok Hossain <farokafs7@gmail.com>
2024-10-08 19:59:04 +0600

    uppercase1stLetter.js added

---
+++ Function/uppercase1stLetter.js
@@ -0,0 +1,9 @@
+const Uppercase = (str) => {
+  let arr1 = str.split(" ");
+  let newArr1 = [];
+  for (let i = 0; i < arr1.length; i++) {
+    newArr1.push(arr1[i].charAt(0).toUpperCase() + arr1[i].slice(1));
+  }
+  return newArr1.join(" ");
+};
+console.log(Uppercase("the quick brown fox"));
