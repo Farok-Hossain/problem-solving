30f13eab54e28d19bbae9d90e85184c6db17d2be
Farok Hossain <farokafs7@gmail.com>
2024-10-11 15:37:12 +0600

    updated fibonacci.js

--- Function/fibonacci.js
+++ Function/fibonacci.js
@@ -1,3 +1,14 @@
+function generateFibonacci(n) {
+  const fibonacci = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+  }
+  return fibonacci;
+}
+
+console.log(generateFibonacci(7));
+
+// generate another way
 const FibonacciSeries = (num) => {
   let num1 = 0;
   let num2 = 1;
@@ -7,7 +18,7 @@ const FibonacciSeries = (num) => {
   } else if (num === 2) {
     return num2;
   } else {
-    for (let i = 2; i <= num; i++) {
+    for (let i = 3; i <= num; i++) {
       sum = num1 + num2;
       num1 = num2;
       num2 = sum;
@@ -16,6 +27,6 @@ const FibonacciSeries = (num) => {
   }
 };
 
-console.log("Fibonacci of 3:", FibonacciSeries(3));
-console.log("Fibonacci of 5:", FibonacciSeries(5));
+console.log("Fibonacci of 3:", FibonacciSeries(2));
+console.log("Fibonacci of 5:", FibonacciSeries(7));
 console.log("Fibonacci of 8:", FibonacciSeries(18));
