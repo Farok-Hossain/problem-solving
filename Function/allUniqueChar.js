2ec04670f039a4613e56e96b6128a29e5bf37a35
Farok Hossain <farokafs7@gmail.com>
2024-10-12 23:10:09 +0600

    allUniqueChar.js added

---
+++ Function/allUniqueChar.js
@@ -0,0 +1,12 @@
+function all_unique_char(st) {
+  let st1 = st;
+  let uniql = "";
+  for (let i = 0; i < st.length; i++) {
+    if (uniql.indexOf(st1.charAt(i)) == -1) {
+      uniql += st1[i];
+    }
+  }
+  return uniql;
+}
+
+console.log(all_unique_char("farokafs"));
