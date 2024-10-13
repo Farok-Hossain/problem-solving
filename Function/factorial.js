f186660bdbe9ea9b8789820d83704fff15bb56c9
Farok Hossain <farokafs7@gmail.com>
2024-10-13 12:37:52 +0600

    updated

--- Function/factorial.js
+++ Function/factorial.js
@@ -7,3 +7,17 @@ const factorial = (n) => {
 };
 
 console.log(factorial(5));
+
+// another way
+const factorial2 = (n) => {
+  let fact = 1;
+  if (n === 0) {
+    return 1;
+  }
+  for (let i = 1; i <= n; i++) {
+    fact = fact * i;
+  }
+  return fact;
+};
+
+console.log(factorial2(4));
