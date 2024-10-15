eeb69ae1f3167fe1b483aaa42f0b096bccac657c
Farok Hossain <farokafs7@gmail.com>
2024-10-15 10:38:26 +0600

    forLoop.js added

---
+++ Function/forLoop.js
@@ -0,0 +1,45 @@
+for (let i = 1; i <= 4; i++) {
+  console.log(i);
+}
+
+for (var j = 1; ; j++) {
+  if (j <= 5) {
+    console.log(j);
+  } else {
+    break;
+  }
+}
+console.log(j);
+
+// for in -> it is used in object
+
+const myObj = {
+  name: "JavaScript",
+  estd: "1995",
+  founder: "Brendon Eich",
+};
+for (property in myObj) {
+  console.log(property);
+}
+
+// for of
+
+const myArray = [1, 2, 3, 4, 5];
+for (element of myArray) {
+  console.log(element);
+}
+
+const myString = "I Love JavaScript!!";
+for (ele of myString) {
+  console.log(ele);
+}
+
+const cars = ["A", "B", "C", "D", "E", "F"];
+console.log(cars.length);
+for (pro of cars) {
+  console.log("Car name:", pro);
+}
+
+for (proo in cars) {
+  console.log(proo);
+}
