7d2cde32806f9ea7e26931554363a4dfad8c31fa
Farok-Hossain <farokafs7@gmail.com>
2024-10-19 22:47:35 +0600

    reverse.js updated

--- Function/reverse.js
+++ Function/reverse.js
@@ -6,3 +6,6 @@ console.log(reverseFunction(1234));
 
 const f = "farok Hossain";
 console.log(f.split("").reverse().join(""));
+
+const p = "I Love Programming!";
+console.log(p.split("").reverse().join(""));
