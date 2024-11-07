08c083457dbe56b1e833c5d42d53e1d1877f8339
Farok Hossain <farokafs7@gmail.com>
2024-11-07 22:32:48 +0600

    fac.js added

---
+++ fac.js
@@ -0,0 +1,20 @@
+function factorial(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
+
+function fibonacci(n) {
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(6));
